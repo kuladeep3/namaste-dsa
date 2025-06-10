780a019925025965a3facf265b809ad0bdf30490
n4ryn <vinaykumar.cs19@aryacollege.in>
2025-06-10 23:25:27 +0530

    Add missing number

---
+++ 03_arrays_lvl_0/08_missing_number.js
@@ -0,0 +1,18 @@
+/** ============================
+ *  Write a function to find the missing number in the given array.
+ * ============================ */
+
+function missingNumber(nums) {
+  let currSum = 0;
+  const actualSum = (nums.length * (nums.length + 1)) / 2;
+
+  for (let i = 0; i < nums.length; i++) {
+    currSum += nums[i];
+  }
+
+  return actualSum - currSum;
+}
+
+// Test Cases:
+console.log(missingNumber([3, 0, 1, 4])); // Output: 2
+console.log(missingNumber([0, 1])); // Output: 2
