4c72145495303d544cd6cdb72d9e286af1d2c6fd
n4ryn <vinaykumar.cs19@aryacollege.in>
2025-06-10 23:57:22 +0530

    Add single number

--- 03_arrays_lvl_0/09_single_number.js
+++ 03_arrays_lvl_0/09_single_number.js
@@ -2,6 +2,7 @@
  *  Write a function to find the single number that appears only once in the given array.
  * ============================ */
 
+// Approach 1: HashMap
 function singleNumber(nums) {
   let hashMap = {};
 
@@ -19,3 +20,8 @@ function singleNumber(nums) {
     }
   }
 }
+
+// Test Cases:
+console.log(singleNumber([2, 2, 1])); // Output: 1
+console.log(singleNumber([4, 1, 2, 1, 2])); // Output: 4
+console.log(singleNumber([1])); // Output: 1
