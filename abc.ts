14d2e371c1136d46ca957bfd7c0f713c0d80dc8c
n4ryn <vinaykumar.cs19@aryacollege.in>
2025-06-13 00:44:14 +0530

    Update Sidebar

---
+++ abc.ts
@@ -0,0 +1,15 @@
+// Random typescript code:
+
+let a: number = 10;
+let b: number = 20;
+
+console.log(a + b);
+
+let c: number = a + b;
+console.log(c);
+
+const d: string = "Hello World";
+console.log(d);
+
+const e: string = d;
+console.log(e);
