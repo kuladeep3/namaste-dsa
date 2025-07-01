6b10c627831f452b90aa8614933dd2f014105ef0
Vinay Kumar <vinaykumar.cs19@aryacollege.in>
2025-07-02 04:50:45 +0530

    feat: add selection sort

---
+++ 05_searching_&_sorting_lvl_0/05_insertion_sort.md
@@ -0,0 +1,1 @@
+# 05. Insertion Sort
---
+++ 05_searching_&_sorting_lvl_0/05_insertion_sort.ts
@@ -0,0 +1,26 @@
+/** ===========================
+ * Write a function that takes an array of integers and returns a sorted array.
+ * ============================ */
+
+function insertionSort(arr: number[]): number[] {
+  // Iterate through the array from the second element
+  for (let i = 1; i < arr.length; i++) {
+    // Initialize the current element and previous element's index
+    let curr = arr[i];
+    let prev = i - 1;
+
+    // Iterate the array until the current element is less than the previous element
+    while (arr[prev] > curr && prev >= 0) {
+      // Swap the current element with the previous element and decrement the previous element's index
+      arr[prev + 1] = arr[prev];
+      prev--;
+    }
+
+    // If the current element is less than the previous element, swap them
+    arr[prev + 1] = curr;
+  }
+
+  return arr;
+}
+
+console.log(insertionSort([7, 4, 3, 5, 1, 2]));
